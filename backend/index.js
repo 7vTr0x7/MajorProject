675b22d6d14aa1d909827e0d2447877e7a3acda7
7vTr0x7 <7vTrox7@gmail.com>
2024-07-19 12:24:32 +0530

    MajorProject - backend - createProduct

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,30 @@ app.get("/products", async (req, res) => {
       res.status(404).json({ error: "Products not Found" });
     }
   } catch (error) {
-    console.log(`Failed to get products: ${error}`);
+    res.status(500).json({ error: `Failed to get products: ${error}` });
+  }
+});
+
+const createProduct = async (prod) => {
+  try {
+    const newProd = new Product(prod);
+    const savedProd = newProd.save();
+    return savedProd;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.post("/products", async (req, res) => {
+  try {
+    const product = await createProduct(req.body);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ error: "Product not Found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: `Failed to create product: ${error}` });
   }
 });
 
