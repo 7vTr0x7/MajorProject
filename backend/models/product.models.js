ab445048a4819a480f221ec43eb19babefbfecf4
7vTr0x7 <7vTrox7@gmail.com>
2024-07-19 12:06:04 +0530

    MajorProject - backend - productModel

---
+++ backend/models/product.models.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    originalPrice: {
+      type: Number,
+      required: true,
+    },
+    discountAvailable: {
+      type: Boolean,
+      required: true,
+    },
+    discountPercent: {
+      type: Number,
+      required: true,
+    },
+  },
+  productImageUrl: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  starRating: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
