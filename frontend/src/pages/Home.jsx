b530ff582da6550ab16c2f45abc8e42d2c0a64f4
7vTr0x7 <7vTrox7@gmail.com>
2024-07-23 13:46:44 +0530

    MajorProject - frontend - Home - fetchCategories

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 
 const Home = () => {
+  const [categories, setCategories] = useState([]);
+
+  const fetchCategories = async () => {
+    try {
+      const res = await fetch("http://localhost:4000/api/categories");
+
+      if (!res.ok) {
+        console.log("Failed to Fetch");
+      }
+
+      const data = await res.json();
+      console.log(data);
+      setCategories(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
   return (
     <>
       <Header />
+      <main className=" container py-4"></main>
     </>
   );
 };
 
 export default Home;
+
+//http://localhost:4000/api/categories
