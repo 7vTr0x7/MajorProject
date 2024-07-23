76fd6c1c8f697b207db3e3588709b610ddcfd8bf
7vTr0x7 <7vTrox7@gmail.com>
2024-07-23 15:07:35 +0530

    MajorProject - frontend - Home - products

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import { useParams } from "react-router-dom";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
   const param = useParams();
-  console.log(param.query ? "data" : "empty");
+
+  const fetchData = async (url) => {
+    try {
+      const res = await fetch(`http://localhost:4000/api/${url}`);
+
+      if (!res.ok) {
+        console.log("Failed to fetch");
+      }
+
+      const data = await res.json();
+      console.log(data);
+      setProducts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!param.query) {
+      fetchData("products");
+    } else if (param.query === "discount") {
+      fetchData("products/product/discount/true");
+    } else {
+      fetchData(`products/product/${param.query}`);
+    }
+  }, [param.query]);
 
   return (
     <>
